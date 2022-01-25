69aef5db5c36cb9f29c0b3aa5d45519350ddba51
sahar-mhd <saharmohammadi@mail.um.ac.ir>
2022-01-25 11:58:20 +0330

    initial

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+if __name__ == '__main__':
+    A, B, C = [int(x) for x in input().split(" ")]
+    n = int(input())
+    L = []
+    for i in range(n):
+        L.append(input().split(" "))
+        L[i][2] = int(L[i][2])
+
+
+
+
