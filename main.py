e70da21e8bd887224b5009c0d0c0d3bc09c73598
fseyedmousavi <faezehseyedmousavi@gmail.com>
2022-01-29 00:22:16 +0330

    final

--- main.py
+++ main.py
@@ -19,7 +19,93 @@ class Task:
 
 
 def HRRN(a, b, c, li):
-    pass
+    T = []
+    for i in range(n):
+        t = Task(l[i][0], l[i][1], l[i][2])
+        T.append(t)
+    w = []
+    j = 0
+    min = 1000
+    m = 0
+    for k in range(len(T)):
+        wt = j
+        s = T[k].duration
+        ratio = (wt + s) / s
+        if ratio < min:
+            min = ratio
+            m = k
+    li.remove(l[m])
+    i = m
+    while len(T) != 0:
+        print("round " + str(j))
+        print("ready queue: " + str(li))
+        print("waiting queue: " + str(w))
+        if T[i].timeSpent == 0:
+            T[i].state = "running"
+            if T[i].type == 'X':
+                # resource A, B
+                if a > 0:
+                    a -= 1
+                else:
+                    w.append(T[i])
+                    T[0].state = "waiting"
+                if b > 0:
+                    b -= 1
+                else:
+                    w.append(T[i])
+                    T[i].state = "waiting"
+            elif T[i].type == "Y":
+                # resource B,C
+                if c > 0:
+                    c -= 1
+                else:
+                    w.append(T[i])
+                    T[0].state = "waiting"
+                if b > 0:
+                    b -= 1
+                else:
+                    w.append(T[i])
+                    T[i].state = "waiting"
+            else:
+                # resource A, C
+                if a > 0:
+                    a -= 1
+                else:
+                    w.append(T[i])
+                    T[i].state = "waiting"
+                if c > 0:
+                    c -= 1
+                else:
+                    w.append(T[i])
+                    T[i].state = "waiting"
+        print("A:" + str(a) + " B:" + str(b) + " C:" + str(c))
+        print("cpu state: ")
+        T[i].timeSpent += 1
+        T[i].print()
+        if T[i].timeSpent == T[i].duration:
+            if T[i].type == 'X':
+                a += 1
+                b += 1
+            elif T[i].type == "Y":
+                b += 1
+                c += 1
+            else:
+                a += 1
+                c += 1
+            T.remove(T[i])
+            min = 1000
+            m = 0
+            for k in range(len(T)):
+                wt = j+1
+                s = T[k].duration
+                ratio = (wt + s) / s
+                if ratio < min:
+                    min = ratio
+                    m = k
+            if len(li)>0:
+                li.remove(li[m])
+            i = m
+        j += 1
 
 
 def SJF(a, b, c, li):
@@ -167,7 +253,7 @@ def RR(a, b, c, l):
         if T[i].timeSpent == T[i].duration:
             T.remove(T[i])
         else:
-            l.append(l[i])
+            l.append([T[i].name, T[i].type, T[i].duration])
             i += 1
         j += 1
 
@@ -187,7 +273,7 @@ if __name__ == '__main__':
     SJF(A, B, C, copy.copy(l))
     print("______________________________")
     print("HRRN")
-    #HRRN(copy.copy(l))
+    HRRN(A, B, C, copy.copy(l))
     print("______________________________")
     print("RR")
     RR(A, B, C, copy.copy(l))
